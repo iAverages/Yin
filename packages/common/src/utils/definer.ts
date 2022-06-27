e637f758be2f6491e7d1558cebbec6886a27ddef
= <31937542+iAverages@users.noreply.github.com>
2022-06-27 21:29:08 +0100

    use null rather than undefined

--- packages/common/src/utils/definer.ts
+++ packages/common/src/utils/definer.ts
@@ -1,5 +1,5 @@
 import Snowflake from "../Snowflake";
 
-export const defineSnowflake = (value: any): Snowflake | undefined => {
-    return value ? new Snowflake(value) : undefined;
+export const defineSnowflake = (value: any): Snowflake | null => {
+    return value ? new Snowflake(value) : null;
 };
