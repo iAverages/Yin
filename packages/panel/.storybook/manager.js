1512d139007b9eba4a4ebbf2bf50ac1d26056282
= <31937542+iAverages@users.noreply.github.com>
2022-07-09 23:51:25 +0100

    Custom theme for storybook

--- packages/panel/.storybook/manager.js
+++ packages/panel/.storybook/manager.js
@@ -1,6 +1,15 @@
 import { addons } from "@storybook/addons";
-import { themes } from "@storybook/theming";
+import { create, themes } from "@storybook/theming";
 
 addons.setConfig({
-    theme: themes.dark,
+    theme: create({
+        base: "dark",
+        brandTitle: "Yin Storybook",
+        appBg: "#2e2e2e",
+        appContentBg: "#262626",
+        barBg: "#262626",
+        barSelectedColor: "white",
+        colorPrimary: "#9c3eba",
+        colorSecondary: "#9c3eba",
+    }),
 });
