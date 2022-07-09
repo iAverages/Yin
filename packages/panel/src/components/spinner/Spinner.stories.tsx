c00e97873f34d417164bb1368c5e058fbb2121ec
= <31937542+iAverages@users.noreply.github.com>
2022-07-09 17:09:22 +0100

    Add controls to Spinner

--- packages/panel/src/components/spinner/Spinner.stories.tsx
+++ packages/panel/src/components/spinner/Spinner.stories.tsx
@@ -1,8 +1,12 @@
-import Spinner from "./Spinner";
+import Spinner, { SpinnerProps } from "./Spinner";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
     title: "Spinner",
     component: Spinner,
-};
+} as ComponentMeta<typeof Spinner>;
 
-export const Default = () => <Spinner />;
+const Template: ComponentStory<typeof Spinner> = (args: SpinnerProps) => <Spinner {...args}>Default Button</Spinner>;
+
+export const Default = Template.bind({});
+Default.args = {};
