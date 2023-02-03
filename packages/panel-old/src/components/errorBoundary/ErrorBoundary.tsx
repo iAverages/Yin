77ce1302c1031bb4d00b7be93e7ee1708133d3f2
iAverages <me@danielraybone.com>
2023-02-03 14:56:41 +0000

    Remove temp panel-old files

--- packages/panel-old/src/components/errorBoundary/ErrorBoundary.tsx
+++
@@ -1,9 +0,0 @@
-import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
-import { FCWithChildren } from "../../types/WithChildren";
-import { FallbackComponent } from "./FallbackComponent";
-
-export const ErrorBoundary: FCWithChildren = ({ children }) => {
-    return <ReactErrorBoundary FallbackComponent={FallbackComponent}>{children}</ReactErrorBoundary>;
-};
-
-export default ErrorBoundary;
