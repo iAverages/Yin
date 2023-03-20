d3bdc1523213d6e5440ed56fc392275cc0ddaa4f
iAverages <31937542+iAverages@users.noreply.github.com>
2023-03-20 18:27:08 +0000

    Setup prettier + import sorting

---
+++ prettier.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import("prettier").Config | import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
+const config = {
+    plugins: [require("@ianvs/prettier-plugin-sort-imports")],
+    printWidth: 120,
+    tabWidth: 4,
+    importOrder: [
+        "^(react/(.*)$)|^(react$)",
+        "^(next/(.*)$)|^(next$)",
+        "<THIRD_PARTY_MODULES>",
+        "",
+        "^types$",
+        "^@/types/(.*)$",
+        "^@/config/(.*)$",
+        "^@/lib/(.*)$",
+        "^@/components/(.*)$",
+        "^@/styles/(.*)$",
+        "^@/(.*)$",
+        "^[./]",
+    ],
+    importOrderSeparation: false,
+    importOrderSortSpecifiers: true,
+    importOrderBuiltinModulesToTop: true,
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+    importOrderMergeDuplicateImports: true,
+    importOrderCombineTypeAndValueImports: true,
+};
+
+module.exports = config;
