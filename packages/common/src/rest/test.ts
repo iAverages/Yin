ff528e625b5589d760c6771be6608a9d529422b7
iAverages <31937542+iAverages@users.noreply.github.com>
2023-04-10 20:25:11 +0100

    Remove old discord related code in common

--- packages/common/src/rest/test.ts
+++
@@ -1,7 +0,0 @@
-import api from "./request";
-
-try {
-    console.log(await api.guilds.auditLogs.get({ "guild.id": "849562494447124500", queryParams: { action_type: 24 } }));
-} catch (e) {
-    console.log(e);
-}
