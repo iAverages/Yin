43f660bbb9c599f865a374415bb38453e155b230
iAverages <31937542+iAverages@users.noreply.github.com>
2023-04-11 03:09:48 +0100

    Don't use import alias for common
    tsup doesnt use import alias for internal packages being transpiled

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 // Always load dotenv when common is imported
 dotenv.config({ path: path.resolve("../../.env") });
 
-import { consts } from "~/consts";
-import { env as __env, globalSchema, validateEnvVars } from "~/env";
+import { consts } from "./consts";
+import { env as __env, globalSchema, validateEnvVars } from "./env";
 
 // __env export is used for other packages, services should not use this export directly
 // instead, use validateEnvVars
