7834dcb006a7852338724201febe874434675d4b
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-25 22:37:58 +0100

    Add grpc metric collection to database service

--- packages/grpc/src/connections.ts
+++ packages/grpc/src/connections.ts
@@ -6,34 +6,54 @@ import { database as databaseService, worker as workerService } from "./index";
 
 const grpcPacketTimings = new metrics.Histogram({
     help: "Time taken to send a packet",
-    name: "grpc_packet_timings",
-    labelNames: ["endpoint"] as const,
+    name: "grpc_request_duration_seconds",
+    labelNames: ["endpoint", "service", "host"] as const,
 });
+
+const createGrpcRequestDurationInterceptor = ({
+    service,
+    options,
+    next,
+}: {
+    service: string;
+    options: grpc.InterceptorOptions;
+    next: grpc.NextCall;
+}) => {
+    return new InterceptingCall(next(options), {
+        start: (metadata, _listener, next) => {
+            const timer = grpcPacketTimings.startTimer();
+            return next(metadata, {
+                onReceiveMessage: (message, next) => {
+                    timer({
+                        endpoint: options.method_definition.path,
+                        service,
+                        host: options.host,
+                    });
+                    return next(message);
+                },
+            });
+        },
+    });
+};
 
 export const createWorkerConnection = () => {
     const worker = new workerService.WorkerClient(__env.YIN_WORKER_GRPC_HOST, grpc.credentials.createInsecure(), {
-        interceptors: [
-            (options, next) => {
-                return new InterceptingCall(next(options), {
-                    start: (metadata, _listener, next) => {
-                        const timer = grpcPacketTimings.startTimer();
-                        return next(metadata, {
-                            onReceiveMessage: (message, next) => {
-                                timer({ endpoint: options.method_definition.path });
-                                return next(message);
-                            },
-                        });
-                    },
-                });
-            },
-        ],
+        interceptors: [(options, next) => createGrpcRequestDurationInterceptor({ service: "worker", options, next })],
     });
 
     return worker;
 };
 
 export const createDatabaseConnection = () => {
-    const worker = new databaseService.DatabaseClient(__env.YIN_DATABASE_GRPC_HOST, grpc.credentials.createInsecure());
+    const database = new databaseService.DatabaseClient(
+        __env.YIN_DATABASE_GRPC_HOST,
+        grpc.credentials.createInsecure(),
+        {
+            interceptors: [
+                (options, next) => createGrpcRequestDurationInterceptor({ service: "worker", options, next }),
+            ],
+        }
+    );
 
-    return worker;
+    return database;
 };
