1012946cb1858646b9687f3dc40a143d02f5c078
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-25 23:28:02 +0100

    Setup database for prometheus metric logging

--- packages/common/src/metrics/prometheus.ts
+++ packages/common/src/metrics/prometheus.ts
@@ -3,10 +3,12 @@ import prometheus from "prom-client";
 
 prometheus.collectDefaultMetrics();
 
-export const createPrometheusServer = () => {
+export const createPrometheusServer = (props?: { additional?: string[] }) => {
+    const additional = props?.additional ?? [];
     const server = http.createServer(async (req, res) => {
         if (req.url === "/metrics") {
-            res.end(await prometheus.register.metrics());
+            res.setHeader("Content-Type", prometheus.register.contentType);
+            res.end((await prometheus.register.metrics()) + additional.join("\n"));
             return;
         }
         res.statusCode = 404;
