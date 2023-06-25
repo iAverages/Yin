0d5af1c1c034f81119508bb40ff93e6551d03f8e
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-25 22:41:21 +0100

    Remove testing code

--- packages/common/src/metrics/prometheus.ts
+++ packages/common/src/metrics/prometheus.ts
@@ -3,17 +3,6 @@ import prometheus from "prom-client";
 
 prometheus.collectDefaultMetrics();
 
-const counter = new prometheus.Counter({ name: "my_counter", help: "my_counter_help" });
-
-(async () => {
-    counter.inc();
-    console.log(await prometheus.register.metrics());
-    counter.inc();
-    console.log(await prometheus.register.metrics());
-    counter.inc();
-    console.log(await prometheus.register.metrics());
-})();
-
 export const createPrometheusServer = () => {
     const server = http.createServer(async (req, res) => {
         if (req.url === "/metrics") {
