01d240b48a292f16c9878eab9ab01b2a17bde4a9
dan <31937542+iAverages@users.noreply.github.com>
2023-06-26 17:34:29 +0100

    Cleanup & test autodeploy (#197)
    * Cleanup debug interaction
    
    * Make prom server use prom port env var

--- packages/common/src/env/schema.ts
+++ packages/common/src/env/schema.ts
@@ -9,6 +9,7 @@ export const globalSchema = z.object({
 
     YIN_DISCORD_TOKEN: z.string(),
     YIN_DEBUG: z.string().optional(),
+    YIN_PROMETHEUS_PORT: z.string().default("8080"),
 
     // Server specific but are shared
     YIN_WORKER_GRPC_HOST: devDefault("localhost:50051"),
