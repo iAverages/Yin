eb21a85ae6545edf425748bdcfe98c27307e6c9a
iAverages <31937542+iAverages@users.noreply.github.com>
2023-06-27 23:45:04 +0100

    Fix sed commit
    Fuck you eslint

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -38,7 +38,8 @@ const main = async () => {
     exec(`git checkout -b ${head}`);
     console.log(`Updating manifest to ${GIT_HASH}`);
     exec(
-        `sed -i 's/image:.*/image: ctr.avrg.dev/yin/${SERVICE_NAME}:${GIT_HASH}/' configs/${SERVICE_NAME}/deployment.yaml`
+        // eslint-disable-next-line no-useless-escape
+        `sed -i 's/image:.*/image: ctr.avrg.dev\/yin\/${SERVICE_NAME}:${GIT_HASH}\/' configs\/${SERVICE_NAME}\/deployment.yaml`
     );
     console.log(`Commiting changes...`);
     exec(`git commit -am "${title}"`);
